74cb48e0b063510a93d63675087cbc74fcb61f7a
Leylaahaciyeva <112489440+Leylaahaciyeva@users.noreply.github.com>
2022-11-21 02:58:09 +0400

    Add files via upload

---
+++ Task9/assets/js/app.js
@@ -0,0 +1,16 @@
+//math
+/*console.log((Math.PI)*(Math.pow(4,2)))*/
+//date
+/*var date= new Date().getHours()
+console.log(date);
+if(date>6 & date<12){
+    document.body.style.backgroundColor="yellow"
+    alert('Sabahiniz Xeyir');
+}else if(date>12 & date<18);{
+    document.body.style.backgroundColor="orange"
+    alert('Gunortaniz Xeyir')
+}else (date>18 & date<6);{
+    document.body.style.backgroundColor="black"
+    alert('Axsaminiz  Xeyir')
+}*/
+
